e3dcb9216c7a101e86ce1d8604636b6dadde6f10
Felipe Moyano <gafemoyano@gmail.com>
2017-02-08 18:39:46 -0500

    add eslint - refactor into styled-components

--- src/PlayerSlider.js
+++ src/PlayerSlider.js
@@ -1,16 +1,16 @@
 import React, { PropTypes } from 'react';
 import styled from 'styled-components'
 import { formatTime } from './utils';
-import { white, halfWhite, black } from './style-variables';
+import { white, halfWhite, black, tablet } from './style-variables';
 
-const rangeWidth = '100% !default';
-const rangeHandleColor =  `${white} !default`;
-const rangeHandleColorHover = `${white} !default`;
-const rangeHandleSize = '18px !default';
-const rangeTrackColor = `${black} !default`;
-const rangeTrackHeight = '2px !default';
-const rangeLabelColor = `${halfWhite} !default`;
-const rangeLabelWidth = '60px !default';
+const rangeWidth = '100%';
+const rangeHandleColor =  `${white}`;
+const rangeHandleColorHover = `${white}`;
+const rangeHandleSize = '18px';
+const rangeTrackColor = `${black}`;
+const rangeTrackHeight = '2px';
+const rangeLabelColor = `${halfWhite}`;
+const rangeLabelWidth = '60px';
 
 const sliderThumb = `
     -webkit-appearance: none;
@@ -45,7 +45,7 @@ const sliderTrack = `
 `;
 
 const InputRange = styled.input`
-  webkit-appearance: none;
+  -webkit-appearance: none;
   background: none;
   display: block;
   position: relative;
@@ -74,7 +74,39 @@ const InputRange = styled.input`
     ${sliderThumb}
   }
 `;
+const SliderTrack = styled.div`
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 50%;
+  background: ${white};
+  height: ${rangeTrackHeight};
+  border-radius: 1px;
+  width: 0%;
+  margin-top: -1px;
+`;
 
+const BelowSection = styled.span`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Title = styled.span`
+  display: none;
+  font-size: 1.15em;
+  font-weight: 900;
+
+  @media (min-width: ${tablet}px) {
+    display: inline;
+  }
+`
+const CurrentTime = styled.output`
+  margin-left: auto;
+  text-align: right;
+  color: ${halfWhite};
+  font-size: 0.75em;
+
+`;
 const PlayerSlider = ({
   trackDuration,
   currentTime,
@@ -85,10 +117,11 @@ const PlayerSlider = ({
   onMouseUp,
 }) => {
 
-  const roundedCurrentTime = Math.round(currentTime || 0)
-  const percentComplete = roundedCurrentTime / trackDuration * 100
-  const formattedCurrentTime = formatTime(currentTime | 0)
-  const formattedDuration = formatTime(trackDuration| 0)
+  const roundedCurrentTime = Math.round(currentTime || 0);
+  const percentComplete = roundedCurrentTime / trackDuration * 100;
+  const formattedCurrentTime = formatTime(currentTime | 0);
+  const formattedDuration = formatTime(trackDuration| 0);
+
   return (
     <form className="podcast-player_slider">
       <div className="range-slider">
@@ -97,7 +130,6 @@ const PlayerSlider = ({
         </span>
         <div className="range-slider_range_wrap">
           <InputRange
-            className="range-slider_range js-player-scrubber"
             type="range"
             value={roundedCurrentTime}
             onChange={onScrubberChange}
@@ -105,19 +137,15 @@ const PlayerSlider = ({
             onMouseUp={onMouseUp}
             min="0"
             max={trackDuration}/>
-          <div
-            className="range-slider_range_track js-player-track"
-            style={{width: `${percentComplete}%`}}
-          >
-          </div>
+          <SliderTrack style={{width: `${percentComplete}%`}} />
         </div>
-        <span className="range-slider_below">
-          <span className="js-player-title">{audioTitle}</span>
-          <output>
-            <b className="js-player-current">{formattedCurrentTime}</b> /
-            <span className="js-player-duration"> {formattedDuration}</span>
-          </output>
-        </span>
+        <BelowSection>
+          <Title>{audioTitle}</Title>
+          <CurrentTime>
+            <b style={{fontWeight: 400, color: white}}>{formattedCurrentTime}</b> /
+            <span> {formattedDuration}</span>
+          </CurrentTime>
+        </BelowSection>
       </div>
     </form>
   );
--- src/style-variables.js
+++ src/style-variables.js
@@ -1,8 +1,8 @@
 // Colors
 export const black = '#121921';
-export const halfBlack = 'rgba($black, 0.5)';
+export const halfBlack = 'rgba(18, 25, 33, 0.5)';
 export const white = '#ffffff';
-export const halfWhite = 'rgba($white, 0.5)';
+export const halfWhite = 'rgba(255, 255, 255, 0.5)';
 export const green = '#61c192';
 export const lightYellow = '#fffeca';
 export const blue = '#2D3589';
@@ -11,3 +11,6 @@ export const mediumGrey = '#878b8f';
 export const darkGrey = '#474b4f';
 export const red = '#ef5125';
 
+export const smartphone = 452;
+export const tablet = 768;
+export const desktop = 1080;
