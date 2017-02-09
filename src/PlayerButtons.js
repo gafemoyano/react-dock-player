c66e3852180071121af0c5826b3f96f0569206ed
Felipe Moyano <gafemoyano@gmail.com>
2017-02-09 09:26:11 -0500

    refactor into styled components

--- src/PlayerButtons.js
+++ src/PlayerButtons.js
@@ -1,13 +1,34 @@
-import React, {PropTypes} from 'react';
-import cx from 'classnames'
+import React, { PropTypes } from 'react';
+import cx from 'classnames';
 import PodcastArrow from './assets/icons/podcast-arrow.svg';
-import styled from 'styled-components';
-import { halfBlack, halfWhite } from './style-variables';
+import styled, { css } from 'styled-components';
+import { white, halfBlack, halfWhite, tablet } from './style-variables';
+import ForwardIcon from './assets/icons/next-15.svg';
+import BackIcon from './assets/icons/prev-15.svg';
+import PlayIcon from './assets/icons/play.svg';
+import PauseIcon from './assets/icons/pause.svg';
+import LoadingIcon from './assets/icons/loading.svg';
+import LoadingIcon2 from './assets/icons/loading-2.svg';
 
-const Button = styled.button`
-  background: ${halfBlack} center no-repeat;
+const Container = styled.div`
+  width: 100%;
+  padding: 1rem 0;
+  display: flex;
+  flex: 1 0 auto;
+  justify-content: center;
+  align-items: center;
+
+  @media (min-width: ${tablet}px) {
+    width: auto;
+    padding: 0 1rem;
+    flex: 0 0 auto;
+  }
+`;
+
+const BaseButton = styled.button`
+  background: rgba(18,25,33,.5) 50% no-repeat;
   border-radius: 200px;
-  color: ${halfWhite};
+  color: hsla(0,0%,100%,.5);
   padding: 0;
   height: 3.85rem;
   width: 3.85rem;
@@ -15,61 +36,117 @@ const Button = styled.button`
   align-content: center;
   justify-content: center;
   text-align: center;
+  cursor: pointer;
+  >img {
+    display: block;
+    margin: 0 0.5rem;
+  }
+  ${props => props.canPlay &&
+  css`
+    &:hover {
+      filter: brightness(115%);
+      color: ${white};
+    }`}
+
 `;
 
+const PlayPostButton = styled(BaseButton)`
+  background-color: transparent;
+  justify-content: space-around;
+  line-height: 1.25rem;
+  > img {
+    height: 1.25rem;
+  }
+  ${props => !props.canPlay &&
+  css`
+      filter: brightness(50%);
+      cursor: default;`}
+`;
+const BackButton = styled(BaseButton)`
+  height: 2.25rem;
+  width: 2.25rem;
+  margin: 0 0.5rem;
+  background-image: url("${BackIcon}");
+`;
 
-const PlayerButtons = ({
-  isPaused,
-  isPlaying,
-  isLoading,
-  onPlayPause,
-  onIncrement,
-  onDecrement,
-}) => {
+const ForwardButton = styled(BaseButton)`
+  height: 2.25rem;
+  width: 2.25rem;
+  margin: 0 0.5rem;
+  background-image: url("${ForwardIcon}");
+`;
+
+const PlayButton = styled(PlayPostButton)`
+  background: rgba(18,25,33,.5) 50% no-repeat;
+  background-image: url("${PlayIcon}");
+  background-size: 18px;
+  ${props => props.isPlaying &&
+  css`
+    background-image: url("${PauseIcon}");
+    background-size: auto;
+  `}
+  ${props => props.isLoading &&
+  css`
+    background-image: url("${LoadingIcon}");
+    animation: spin 1s ease-in-out infinite;
+    &:after {
+      animation: spin 1s ease-in-out infinite;
+      content: "";
+      background: url("${LoadingIcon2}") center no-repeat;
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+    }
+  `}
+`;
+
+const PlayerButtons = (
+  {
+    isPaused,
+    isPlaying,
+    isLoading,
+    prevPost,
+    nextPost,
+    onPlayPause,
+    onIncrement,
+    onDecrement,
+  },
+) => {
   const playButtonClasses = cx(
     'podcast-player-button',
     'podcast-player-button--play',
     'js-player-play-button',
-    {'is-paused': isPaused},
-    {'is-playing': isPlaying},
-    {'is-loading': isLoading},
-  )
+    { 'is-paused': isPaused },
+    { 'is-playing': isPlaying },
+    { 'is-loading': isLoading },
+  );
   return (
-    <div className="podcast-player_buttons">
-      <button
-        className="podcast-player-button podcast-player-button--episode js-player-prev-button"
-        title="Listen to the previous episode">
-        <span
-          className="js-player-prev-number">
-        </span>
-        <img src={PodcastArrow} alt="Previous episode"/>
-      </button>
-      <button
-        className="podcast-player-button podcast-player-button--back15 js-player-back-button"
-        title="Seek back 15 seconds"
-        onClick={onDecrement.bind(15)}>
-      </button>
-
-      <button
-        className={playButtonClasses}
-        onClick={onPlayPause.bind(15)}>
-      </button>
-
-      <button
-        className="podcast-player-button podcast-player-button--forward15 js-player-forward-button"
-        title="Seek forward 15 seconds"
-        onClick={onIncrement}>
-      </button>
-      <button className="podcast-player-button podcast-player-button--episode js-player-next-button" title="Listen to the next episode">
+    <Container>
+      <PlayPostButton canPlay={prevPost} title="Listen to the previous post">
+        <span>{prevPost && `#${prevPost}`}</span>
+        <img src={PodcastArrow} alt="Previous post" />
+      </PlayPostButton>
+      <BackButton title="Seek back 15 seconds" onClick={onDecrement.bind(15)} />
+      <PlayButton
+        isPlaying={isPlaying}
+        isPaused={isPaused}
+        isLoading={isLoading}
+        style={{padding: 0}}
+        onClick={onPlayPause.bind(15)}
+      />
+      <ForwardButton title="Seek forward 15 seconds" onClick={onIncrement} />
+      <PlayPostButton canPlay={nextPost} title="Listen to the next post">
         <img
-          className="flip-horizontal"
           src={PodcastArrow}
-          alt="Next episode"
+          alt="Next post"
+          style={{ transform: 'scaleX(-1)' }}
         />
-          <span className="js-player-next-number"></span>
-      </button>
-    </div>
-  )
+        <span>{nextPost && `#${nextPost}`}</span>
+      </PlayPostButton>
+    </Container>
+  );
 };
 
 PlayerButtons.propTypes = {
--- src/index.js
+++ src/index.js
@@ -1,13 +1,67 @@
 import './assets/sass/app.sass';
-// import './player.css'
 import cx from 'classnames';
+import styled, { css } from 'styled-components';
 import React, { Component, PropTypes } from 'react';
 import AudioWrapper from './AudioWrapper';
 import PlayerNav from './PlayerNav';
 import PlayerDetails from './PlayerDetails';
 import PlayerButtons from './PlayerButtons';
 import PlayerSlider from './PlayerSlider';
+import { tablet, white } from './style-variables';
+const PlayerContainer = styled.div`
+  display: flex;
+  & *{
+    box-sizing: border-box;
+   }
+  & *:focus{
+    outline: none !important;
+  }
+  & button {
+    cursor: pointer;
+    border: none;
+    max-width: 100%;
+    overflow-x: hidden;
+    margin: 0;
+    padding: 1rem;
+  }
+`;
 
+const Player = styled.figure`
+  filter: blur(5px);
+  opacity: 0;
+  pointer-events: none;
+  transition: all 0.1s ease-in-out 0s;
+  transform: translateY(100%) scale(1.0001) translate3d(0, 0, 0);
+  visibility: hidden;
+  will-change: transform opacity;
+  background: rgba(18, 25, 33, 0.95);
+  bottom: 0;
+  color: ${white};
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0;
+  position: fixed;
+  width: 100%;
+  z-index: 1000;
+  @media (min-width: ${tablet}px) {
+    flex-wrap: nowrap;
+    align-items: center;
+  }
+
+  ${props => props.isActive &&
+  css`
+      filter: blur(0);
+      opacity: 1;
+      pointer-events: inherit;
+      transform: translateY(0) scale(1) translate3d(0, 0, 0);
+      visibility: visible;
+    `}
+
+    ${props => props.isHidden &&
+  css`
+    transform: translateY(100%) scale(1.0001);
+  `}
+`;
 export default class DockPlayer extends Component {
   static propTypes = {
     audioTitle: PropTypes.string,
@@ -132,7 +186,7 @@ export default class DockPlayer extends Component {
       { 'podcast-player--is-hidden': this.state.isHidden },
     );
     return (
-      <div className="player-container">
+      <PlayerContainer>
         <AudioWrapper
           ref={node => this.audio = node}
           source={this.props.audioUrl}
@@ -150,38 +204,36 @@ export default class DockPlayer extends Component {
           onSeeking={this.handleSeeking}
           onSeeked={this.handleSeeked}
         />
-        <div id="player">
-          <figure className={playerClasses}>
-            <PlayerNav
-              isHidden={this.state.isHidden}
-              togglePlayer={this.togglePlayer}
-              close={this.close}
-            />
-            <PlayerArt source={this.props.artUrl} />
-            <PlayerDetails
-              title={this.props.playerTitle}
-              audioTitle={this.props.audioTitle}
-            />
-            <PlayerButtons
-              isPaused={this.state.isPaused}
-              isPlaying={this.state.isPlaying}
-              isLoading={this.state.isLoading}
-              onPlayPause={this.handlePlayPause}
-              onIncrement={this.handleIncrement}
-              onDecrement={this.handleDecrement}
-            />
-            <PlayerSlider
-              trackDuration={this.state.trackDuration}
-              currentTime={this.state.displayTime}
-              title={this.props.playerTitle}
-              audioTitle={this.props.audioTitle}
-              onScrubberChange={this.handleScrubberChange}
-              onMouseDown={this.handleScrubberMouseDown}
-              onMouseUp={this.handleScrubberMouseUp}
-            />
-          </figure>
-        </div>
-      </div>
+        <Player isActive={true} isHidden={this.state.isHidden}>
+          <PlayerNav
+            isHidden={this.state.isHidden}
+            togglePlayer={this.togglePlayer}
+            close={this.close}
+          />
+          <PlayerArt source={this.props.artUrl} />
+          <PlayerDetails
+            title={this.props.playerTitle}
+            audioTitle={this.props.audioTitle}
+          />
+          <PlayerButtons
+            isPaused={this.state.isPaused}
+            isPlaying={this.state.isPlaying}
+            isLoading={this.state.isLoading}
+            onPlayPause={this.handlePlayPause}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+          />
+          <PlayerSlider
+            trackDuration={this.state.trackDuration}
+            currentTime={this.state.displayTime}
+            title={this.props.playerTitle}
+            audioTitle={this.props.audioTitle}
+            onScrubberChange={this.handleScrubberChange}
+            onMouseDown={this.handleScrubberMouseDown}
+            onMouseUp={this.handleScrubberMouseUp}
+          />
+        </Player>
+      </PlayerContainer>
     );
   }
 }
